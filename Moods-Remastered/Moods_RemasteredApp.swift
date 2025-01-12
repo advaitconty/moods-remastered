35a2e0efa9ad7878cd361c9ba5e7ebdb7adac2d4
AdvaitThePro <advaitthepro@outlook.com>
2025-01-12 15:35:45 +0800

    Initial Commit

---
+++ Moods-Remastered/Moods_RemasteredApp.swift
@@ -0,0 +1,17 @@
+//
+//  Moods_RemasteredApp.swift
+//  Moods-Remastered
+//
+//  Created by Milind Contractor on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Moods_RemasteredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
